e3ca8db98ecde825c2cb15383da4ec81cddaae87
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-05 03:29:12 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "help":
         await query.message.edit_text(
-            text = f"<b>Bot Cammands\n❏ Commands For BOT Admins\n├/start : start the bot or get posts\n├/batch : create link for more than one posts\n├/genlink : create link for one post\n├/users : view bot statistics\n├/broadcast : broadcast any messages to bot users\n└/stats : checking your bot uptime\n\n👨‍💻 Developed by <a href=https://t.me/anime_bash>Anime Bash</a></b>",
+            text = f"<b>Bot Cammands\n❏ Commands For BOT Admins\n├/start : start the bot or get posts\n├/batch : create link for more than one posts\n├/genlink : create link for one post\n├/users : view bot statistics\n├/broadcast : broadcast any messages to bot users\n└/stats : checking your bot uptime\n\n👨‍💻 Developed by <a href=https://t.me/Dzayu>Admin surga</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
@@ -23,7 +23,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         )
     elif data == "about":
         await query.message.edit_text(
-            text = f"<b>Creator - @Rcbkafan\nMy Channel - @Anime_bash\nDicsussion Group - @anime_bash_chat</a>\n\n👨‍💻 Developed by @Officialcreater</b>",
+            text = f"<b>Creator - @kotol\nMy Channel - @ResahBerkata\nDicsussion Group - @logguaaja</a>\n\n👨‍💻 Developed by memek kuda</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
